cd8cf1e78a9ffa311807439d6fe9a619b4ceafd3
DavidBThomp <65555644+DavidBThomp@users.noreply.github.com>
2020-05-26 13:00:43 -0500

    Update Activity 2.js

--- Assignment 3/Activity 2.js
+++ Assignment 3/Activity 2.js
@@ -1,17 +1,12 @@
 //This program is to appoximatley convert age into months, days, hours, and seconds
 var age = prompt("Enter your age in years", "20");
 
-var s = age * 31536000;
-var h = age * 8760;
-var d = age * 365;
-var m = age * 12;
+var seconds = age * 31536000;
+var hours = age * 8760;
+var days = age * 365;
+var months = age * 12;
 
-var ss = "age in seconds:";
-var hh = "age in hours:";
-var dd = "age in days:";
-var mm = "age in months:";
-
-console.log(ss,s);
-console.log(hh, h);
-console.log(dd, d);
-console.log(mm, m);
+console.log("age in seconds",seconds);
+console.log("age in hours", hours);
+console.log("age in days", days);
+console.log("age in months", months);
