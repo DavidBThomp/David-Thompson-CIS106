1f837e4b501cae79da4ce64360da009d328a1d62
Dave Braunschweig <dbraunsc@harpercollege.edu>
2020-06-17 11:18:40 -0500

    Assignment 9 corrections

--- Assignment 9/Activity 1.js
+++ Assignment 9/Activity 1.js
@@ -11,17 +11,17 @@ function instructions() {
     console.log("Please input scores, when done input any negative number to calculate average.");
 }
 
-
 function getGrade() {
     let count = 0;
     let total = 0;
     
     do {
         scores = prompt('Enter a value for scores');
-        if (scores > 0) {
-            total = Number(total) + Number(scores);
+        if (scores >= 0) {
+            total = total + Number(scores);
             count = count + 1;
         }
-    } while (scores > -1);
-    console.log("The average is " + total/count + ".");
+    } while (scores >= 0);
+    average = total / count;
+    console.log("The average is " + average + ".");
 }
--- Assignment 9/Activity 3.js
+++ Assignment 9/Activity 3.js
@@ -5,10 +5,10 @@ main();
 function main() {
 	do {
 		let mile = getMile();
-		processDistance(mile);
-		choice = pickUnit(yard, foot, inches, kilometer, meters, centimeters);
+		choice = pickUnit();
+        processDistance(mile, choice);
 		repeat = doRepeat();
-	} while (repeat == "Y");
+	} while (repeat == "Y" || repeat == "y");
 }
 
 function getMile() {
@@ -16,30 +16,60 @@ function getMile() {
 	return mile;
 }
 
-function processDistance(mile) {
-	yard = mile * 1760;
-	foot = mile * 5280;
-	inches = mile * 63360;
-	kilometer = mile * 1.609344;
-	meters = kilometer * 1000;
-	centimeters = meters * 100;
-	return yard, foot, inches, kilometer, meters, centimeters;
-}
-
-function pickUnit(yard, foot, inches, kilometer, meters, centimeters) {
-	choice = prompt("Would you like that distance in US or metric units?");
+function processDistance(mile, choice) {
 	switch (choice) {
 		case ('US'):
 		case ('us'):
+            let yard = calculateYard(mile);
+            let foot = calculateFoot(mile);
+            let inches = calculateInches(mile);
 			console.log("That is " + yard + " yards, " + foot + " feet, and " + inches + " inches.");
 			break;
 		case ('metric'):
 		case ('Metric'):
+            let kilometer = calculateKilometer(mile);
+            let meters = calculateMeters(kilometer);
+            let centimeters = calculateCentimeters(meters);
 			console.log("That is " + kilometer + " kilometers, " + meters + " meters, and " + centimeters + " centimeters.");
 			break;
 		default:
 			console.log("Please enter \'US\' or \'Metric\'.");
 	}
+}
+
+function calculateYard(mile) {
+    let yard = mile * 1760;
+    return yard;
+}
+
+function calculateFoot(mile) {
+	let foot = mile * 5280;
+    return foot;
+}
+
+function calculateInches(mile) {
+	let inches = mile * 63360;
+    return inches;
+}
+
+function calculateKilometer(mile) {
+	let kilometer = mile * 1.609344;
+    return kilometer;
+}
+
+function calculateMeters(kilometer) {
+	let meters = kilometer * 1000;
+    return meters;
+}
+
+function calculateCentimeters(meters) {
+	let centimeters = meters * 100;
+    return centimeters;
+}
+
+function pickUnit() {
+	choice = prompt("Would you like that distance in US or metric units?");
+    return choice;
 }
 
 function doRepeat() {
