3f1b7fbc8e8aaa2a94aea8d43201a137c6c72c56
DavidBThomp <65555644+DavidBThomp@users.noreply.github.com>
2020-06-23 11:28:39 -0500

    Update Activity 1 Fixed Length.js

--- Assignment 11/Activity 1 Fixed Length.js
+++ Assignment 11/Activity 1 Fixed Length.js
@@ -6,15 +6,18 @@ function main() {
     instructions();
     let amount = getAmount();
     let grades = getGrades(amount);
-    displayAverage(grades);
+    let order = displayOrder(grades);
+    arrayMax(grades);
+    arrayMin(grades);
+    arrayAverage(order, amount);
 }
 
 function instructions() {
-    console.log("This program will average out a set of numbers.")
+    console.log("This program will average out a set of grades.")
 }
 
 function getAmount() {
-    amount = prompt("How many numbers will be entered.");
+    amount = prompt("How many grades will be entered.");
     return amount;
 }
 
@@ -22,18 +25,37 @@ function getGrades(amount) {
     let total = 0;
     let count = 0;
     grades = [];
-    console.log("Please enter scores.");
+    console.log("Please enter grades.");
     while (count < amount) {
         scores = prompt("");
         count = count + 1;
         total += Number(scores);
         grades.push(Number(scores));
-        console.log(grades);
     }
     return grades;
 }
 
-function displayAverage(grades) {
-    const arrayGrades = grades
-    console.log(Math.min(arrayGrades))
+function displayOrder(grades) {
+    let order = grades.sort(function(a, b){return a-b});
+    console.log("The grades sorted from lowest to highest are " + order + ".");
+    return order;
 }
+
+function arrayMax(grades){
+    let max = Math.max.apply(null, grades);
+    console.log("The maximum grade is " + max + ".")
+}
+
+function arrayMin(grades){
+    let min = Math.min.apply(null, grades);
+    console.log("The minimum grade is " + min + ".")
+}
+
+function arrayAverage(order, amount){
+    var sum = 0
+    for (var i = 0; i<order.length; i++){
+        sum += order[i]
+    }
+    let average = (sum / amount)
+    console.log("The average grade is " + average + ".")
+    }
