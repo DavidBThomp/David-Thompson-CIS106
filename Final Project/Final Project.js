dc6a40d19aa9bec2101768f7d1e5b6dae5842cd4
DavidBThomp <65555644+DavidBThomp@users.noreply.github.com>
2020-07-05 13:40:35 -0500

    Update Final Project.js

--- Final Project/Final Project.js
+++ Final Project/Final Project.js
@@ -1,26 +1,3 @@
-//Final Project - XML Page
-
-//Time Line
-//Thursday : Begin getting input from website X
-//Friday : Website input fully functional (validate website exists and data from site is good) X
-//Saturday : Begin Processing website data into arrays (name, description, calories, price) 
-//Sunday : Validating Website data (Caps, whitespace, integers vs strings)
-//Monday: Display Data as "name - description - calories - price"
-//Tuesday : Total values and display totals "0 items - 0 average calories - $0.00 average price"
-//Confirm project works and final submission
-
-//Actual Time
-//Thursday : Request to see if page is functional, then take XML from page.
-//Friday : Can not get a way to convert the XML string into readable xml for JavaScript...
-//Saturday : Managed to get values of data I need but have a lot of whitespace elements in array. Will remove white space tonight.
-
-
-//Refrences
-//Extracting XML file: https://stackoverflow.com/questions/50025134/how-to-extract-xml-data-from-a-url-link-with-jquery-or-javascript/50025854
-//Reading Headers : https://www.w3schools.com/js/js_ajax_http.asp
-//Getting Tags from string: https://stackoverflow.com/questions/11398419/trying-to-use-the-domparser-with-node-js
-//
-
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 main();
@@ -28,6 +5,11 @@ main();
 function main() {
     let text = requestPage();
     let array = getArray(text);
+    let name = getName(array)
+    let description = getDescription(array);
+    let calorie =  getCalorie(array)
+    let price =  getPrice(array)
+    console.log(name, description, calorie,price)
 }
 
 function requestPage() {
@@ -40,18 +22,51 @@ function requestPage() {
 
 function getArray(text) {    
     var fixfile = text.replace(/\r\n\s+/g, "");
-    var breakText = fixfile.split('>')
+    var breakText = fixfile.split('>');
     breakText.splice(0, 2);
-    breakText.pop()
-    breakText.pop() 
+    breakText.pop();
+    breakText.pop() ;
 
-    var goodarray = []
+    var goodarray = [];
     var holder;
       for(var i = 0; i < breakText.length; i += 1) { 
         holder = breakText[i].split("<")[0];
         goodarray.push(holder);
       }
-    var array = goodarray.filter(String)
-    console.log(array);
+    var array = goodarray.filter(String);
     return array;
 }
+
+function getName(array) {
+    var name = [];
+    for(var i = 0; i < array.length; i += 4) { 
+      name.push(String(array[i]));
+    }
+    return name;
+}
+
+function getDescription(array) {
+  var description = [];
+  for(var i = 2; i < array.length; i += 4) { 
+    description.push(String(array[i]));
+  }
+  return description;
+}
+
+function getCalorie(array) {
+  var calorie = [];
+  for(var i = 3; i < array.length; i += 4) { 
+    calorie.push(Number(array[i]));
+  }
+  return calorie;
+}
+
+function getPrice(array) {
+  var price = [];
+  for(var i = 1; i < array.length; i += 4) { 
+    price.push(Number(array[i]));
+  }
+  return price;
+}
+
+//price has dollar sign, so number is NaN
